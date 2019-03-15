fc3940cbb50da68cbdcbcf11a545843d48c4a0a2
Jaid <jaid.jsx@gmail.com>
2019-03-15 10:57:25 +0100

    Removed debugging code
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react"
 import ReactDom from "react-dom"
 import App from "components/App"
 
-console.log(2)
 const reactNode = document.createElement("div")
-reactNode.id = "react"
 document.body.append(reactNode)
 ReactDom.render(<App/>, reactNode)
